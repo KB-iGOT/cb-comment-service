75f193aecd1a0d885858e553bddb77f4fc1a3616
ruksanasemeir <ruksana.semeir@tarento.com>
2025-05-22 15:32:55 +0530

    prod redis issue resolved

--- src/main/java/com/tarento/commenthub/utility/RedisConfiguration.java
+++ src/main/java/com/tarento/commenthub/utility/RedisConfiguration.java
@@ -12,10 +12,10 @@ import redis.clients.jedis.JedisPoolConfig;
 @EnableCaching
 public class RedisConfiguration {
 
-  @Value("${spring.redis.host}")
+  @Value("${kp.redis.host}")
   private String redisHost;
 
-  @Value("${spring.redis.port}")
+  @Value("${kp.redis.port}")
   private int redisPort;
 
   @Bean
