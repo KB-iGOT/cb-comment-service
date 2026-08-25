5395b592493968bca1dc340d85417c96495d7a2f
ruksanasemeir <ruksana.semeir@tarento.com>
2026-08-25 16:26:28 +0530

    KB-15270 | BE| TECHDEBT| JUnit test cases to improve the code coverage for the cb-comment-service.

--- src/main/java/com/tarento/commenthub/dto/CommentsResoponseDTO.java
+++ src/main/java/com/tarento/commenthub/dto/CommentsResoponseDTO.java
@@ -68,7 +68,6 @@ public class CommentsResoponseDTO {
   public CommentsResoponseDTO(List<Comment> comments,
       List<Object> userList, List<Object> taggedUsers
       , String commentTreeId) {
-    this.commentTree = commentTree;
     this.comments = comments;
     this.users = userList;
     this.taggedUsers = taggedUsers;
