dc658ae777a9c4b54ad00273a200d4da6fa9bd7c
abhishek-kurahatti <abhishek.kurahatti@tarento.com>
2026-09-07 20:36:10 +0530

    Add SonarValidationTest class with basic methods

---
+++ src/main/java/com/tarento/commenthub/utility/SonarValidationTest.java
@@ -0,0 +1,12 @@
+package com.tarento.commenthub.utility;
+
+public class SonarValidationTest {
+
+    public String getMessage() {
+        return "Quality Gate Validation";
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
