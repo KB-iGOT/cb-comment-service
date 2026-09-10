e95007bc5dcaaeddb87cf48063d4d52c5eb4d359
rajeevsathish <rajeev.sathish@tarento.com>
2026-09-10 12:32:31 +0530

    changes for sonarTest

--- src/main/java/com/tarento/commenthub/utility/CommentsUtility.java
+++ src/main/java/com/tarento/commenthub/utility/CommentsUtility.java
@@ -1,8 +1,12 @@
 package com.tarento.commenthub.utility;
 
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CommentsUtility {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(CommentsUtility.class);
 
   public static boolean containsNull(List<?> list) {
     if (list == null) {
@@ -20,12 +24,17 @@ public class CommentsUtility {
 
   // TEMP: dummy method with no test coverage, added to verify Sonar new-code coverage gate
   public static String classifyCount(int count) {
+    String Result_Value;
     if (count <= 0) {
-      return "none";
+      LOGGER.info("classifyCount: count={} classified as none", count);
+      Result_Value = "none";
     } else if (count < 10) {
-      return "few";
+      LOGGER.info("classifyCount: count={} classified as few", count);
+      Result_Value = "few";
     } else {
-      return "many";
+      LOGGER.info("classifyCount: count={} classified as many", count);
+      Result_Value = "many";
     }
+    return Result_Value;
   }
 }
